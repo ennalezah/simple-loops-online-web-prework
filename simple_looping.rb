56431bce580f2a8ed6508f55e185de6b053c010c
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-04-04 02:56:35 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,7 +10,11 @@ def loop_iterator(number_of_times)
   #  phrase and break when the counter hits the appropriate number...
   
   loop do
-  phrase = "Welcome to Flatiron School's Web Development Course!"
+    phrase = "Welcome to Flatiron School's Web Development Course!"
+    counter += 1
+    break if counter == number_of_times
+    end
+  end
   
 end
 
